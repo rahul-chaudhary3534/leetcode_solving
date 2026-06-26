a1ae7b4c55f1a88c614babe4bb825754b16fce48
RAHUL CHAUDHARY <rahul20063534@gmail.com>
2026-06-26 22:17:07 +0530

    Time: 1 ms (96.45%), Space: 42.4 MB (86.21%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n==0)return false;
+        if(n<0)return false;
+        while(n != 0){
+           
+            if (n==1)return true;
+            
+            if(n%2==0) n = n/2;
+            else return false;
+        }
+        return true;
+        
+    }
+}
