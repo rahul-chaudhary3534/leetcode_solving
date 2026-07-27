8a6e277321b1a4d15dba233a0d4473bf9fcfaa87
RAHUL CHAUDHARY <rahul20063534@gmail.com>
2026-07-27 10:12:38 +0530

    Time: 0 ms (100%), Space: 44.5 MB (83.69%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+
+        int l = 0;
+        int r = n - 1;
+        int ans = n;
+
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] >= target) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+}
